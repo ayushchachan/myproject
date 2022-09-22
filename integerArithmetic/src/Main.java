8b4c7c8adcb127ad4a4240abd5a3fbb48b85407a
Ayush Chachan <ayush.chachan@helloverify.com>
2022-09-22 13:17:05 +0530

    IntelliJ projects

--- integerArithmetic/src/Main.java
+++ integerArithmetic/src/Main.java
@@ -1,20 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        String a = "34";
-        String b = "58";
-        System.out.println("a = " + a + ", b = " + b);
-        System.out.print("a - b = ");
-        MyBigInteger T = new MyBigInteger();
-        String ab = T.IntegerSubtraction(a, b);
-        System.out.println(ab);
-
-        a = "92964789756415219848485649846516589416519891865";
-        b = "91456485464321644645878932121214654584651486484";
-
-        System.out.println("a = " + a + ", b = " + b);
-        System.out.print("a + b = ");
-        ab = T.integerAddition(a, b);
-        System.out.println(ab);
+        char c1 = '2';
+        char c2 = '3';
+        System.out.println(Character.getNumericValue(c1) + Character.getNumericValue(c2));
     }
 }
