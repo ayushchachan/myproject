474b8cba29b7830a744bc5aa97795d503ee6a249
Ayush Chachan <ayush.chachan@helloverify.com>
2023-01-18 14:40:17 +0530

    updated

---
+++ seam carving/src/SeamCarver.java
@@ -0,0 +1,35 @@
+import edu.princeton.cs.algs4.Picture;
+
+public class SeamCarver {
+    public SeamCarver(Picture inputImg) {
+    }
+
+    public int[] findHorizontalSeam() {
+        return null;
+    }
+
+    public void removeVerticalSeam(int[] verticalSeam) {
+    }
+
+    public Picture picture() {
+        return null;
+    }
+
+    public int[] findVerticalSeam() {
+        return null;
+    }
+
+    public void removeHorizontalSeam(int[] horizontalSeam) {
+    }
+
+    public int width() {
+        return 0;
+    }
+
+    public int height() {
+        return 0;
+    }
+
+    public double energy(int col, int row) {
+    }
+}
